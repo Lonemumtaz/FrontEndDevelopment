bc4fd582b91168c23e19b513847b36f6e6abacb5
Mumtaz Lone <mumtazlone1315@gmail.com>
2025-02-19 16:47:33 +0530

    Add files via upload

---
+++ practicequestiom1.js
@@ -0,0 +1,7 @@
+const product = {
+    titile : "ballpen",
+    rating :4,
+    offer:5,
+    price:250
+};
+console.log(product)
---
+++ practicequestion2.js
@@ -0,0 +1,7 @@
+const profile={
+    username: "@mumtazlone",
+    isfollow : true,
+    following:136,
+    followers:390
+};
+console.log(profile);
